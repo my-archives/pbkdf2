838448e796633e6f1f970d609c5d06050216e1ab
fundon <cfddream@gmail.com>
2014-04-18 17:44:47 +0800

    use jshint check code

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
+'use strict';
+
 // http://en.wikipedia.org/wiki/PBKDF2
 // https://www.ietf.org/rfc/rfc6070.txt
 
-var crypto      = require("crypto");
+/**
+ *  Module dependences.
+ */
+
+var crypto      = require('crypto');
 var randomBytes = crypto.randomBytes;
 var pbkdf2      = crypto.pbkdf2;
 var pbkdf2Sync  = crypto.pbkdf2Sync;
 
 /**
- *
- * p: password
- * s: salt
- * c: iterations
- * a: algorithm
- * h: passwordHash
- * keylen
- * saltlen
- *
+ *  Expose
+ */
+
+exports.generateSalt      = generateSalt;
+exports.hash              = hash;
+exports.compare           = compare;
+exports.generateSaltSync  = generateSaltSync;
+exports.hashSync          = hashSync;
+exports.compareSync       = compareSync;
+
+/**
+ *  p: password
+ *  s: salt
+ *  c: iterations
+ *  a: algorithm
+ *  h: passwordHash
+ *  keylen
+ *  saltlen
  */
 
 function generateSalt(cb, saltlen) {
@@ -36,13 +51,13 @@ function hash(p, s, c, keylen, a, cb) {
     var h = buf.toString('hex');
     cb(h);
   });
-};
+}
 
 function compare(h, p, s, c, keylen, a, cb) {
   hash(p, s, c, keylen, a, function (buf) {
     cb(h === buf.toString('hex'));
   });
-};
+}
 
 function generateSaltSync(saltlen) {
   var s = '';
@@ -58,19 +73,8 @@ function hashSync(p, s, c, keylen, a) {
   if (!keylen) keylen = 64;
   if (!a) a = 'sha256';
   return pbkdf2Sync(p, s, c, keylen / 2, a).toString('hex');
-};
+}
 
 function compareSync(h, p, s, c, keylen, a) {
   return h === hashSync(p, s, c, keylen, a);
-};
-
-/**
- *  Expose
- */
-
-exports.generateSalt      = generateSalt;
-exports.hash              = hash;
-exports.compare           = compare;
-exports.generateSaltSync  = generateSaltSync;
-exports.hashSync          = hashSync;
-exports.compareSync       = compareSync;
+}
